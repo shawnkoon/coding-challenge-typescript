5fc203f6c91aaab40bd3451cf937b3b3a15079c4
Shawn Son <shawn9405@gmail.com>
2018-05-14 20:35:13 -0700

    Add anagram question & solutions

---
+++ src/anagram/anagram.test.ts
@@ -0,0 +1,28 @@
+// Lib
+import { expect } from 'chai';
+
+// App
+import { isAnagram } from './anagram';
+
+describe('Anagram', () => {
+  const str1: string = 'aa bb cc';
+  const str2: string = 'ab bcac';
+
+  const str3: string = 'Rail safety';
+  const str4: string = 'fairy tales';
+
+  const str5: string = 'hh ii jj';
+  const str6: string = 'hh ik jj';
+
+  it(`should return true for '${str1}' and '${str2}'.`, () => {
+    expect(isAnagram(str1, str2)).to.equal(true);
+  });
+
+  it(`should return true for '${str3}' and '${str4}'.`, () => {
+    expect(isAnagram(str3, str4)).to.equal(true);
+  });
+
+  it(`should return false for '${str5}' and '${str6}'.`, () => {
+    expect(isAnagram(str5, str6)).to.equal(false);
+  });
+});
---
+++ src/anagram/anagram.ts
@@ -0,0 +1,48 @@
+/**
+ * Anagram.
+ *
+ * When two arguments uses same exact variance of characters and
+ * uses same exact number of those characters.
+ * Exclude punchuations, spaces, and special characters.
+ * Case insensitive.
+ *
+ * ex)
+ * 'aa bb cc', 'ab bc ac' = True
+ * 'Rail safety', 'fairy tales' = True
+ * 'hh ii jj', 'hh ik jj' = False
+ */
+
+interface WordMap {
+  [letter: string]: number | undefined;
+}
+
+export const isAnagram = (str1: string, str2: string): boolean => {
+  const map1: WordMap = getMap(str1);
+  const map2: WordMap = getMap(str2);
+
+  return isCompose(map1, map2) && isCompose(map2, map1);
+};
+
+const isCompose = (from: WordMap, to: WordMap): boolean => {
+  for (const letter in from) {
+    if (!to[letter]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const getMap = (str: string): WordMap => {
+  const map: WordMap = {};
+
+  for (const char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    if (map[char]) {
+      map[char]!++;
+    } else {
+      map[char] = 1;
+    }
+  }
+
+  return map;
+};
